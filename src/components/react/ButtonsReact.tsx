30293de97e5ad46dee23ecbc6abb8cf6f62e1653
Emilio Navarro <sainfuw@outlook.com>
2024-06-30 15:50:16 -0400

    shadcn with astro basic installation

---
+++ src/components/react/ButtonsReact.tsx
@@ -0,0 +1,25 @@
+import { ChevronRightIcon, EnvelopeOpenIcon, ReloadIcon } from "@radix-ui/react-icons";
+import { Button } from "../ui/button";
+
+export function ButtonsReact() {
+  return (
+    <div className="flex gap-2 flex-wrap">
+      <Button onClick={() => alert("click")}>Click Me!</Button>
+      <Button variant="secondary">Click Me!</Button>
+      <Button variant="destructive">Click Me!</Button>
+      <Button variant="outline">Click Me!</Button>
+      <Button variant="ghost">Click Me!</Button>
+      <Button variant="link">Click Me!</Button>
+      <Button variant="outline" size="icon">
+        <ChevronRightIcon className="h-4 w-4" />
+      </Button>
+      <Button>
+        <EnvelopeOpenIcon className="mr-2 h-4 w-4" /> Login with Email
+      </Button>
+      <Button disabled>
+        <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+        Please wait
+      </Button>
+    </div>
+  )
+}
