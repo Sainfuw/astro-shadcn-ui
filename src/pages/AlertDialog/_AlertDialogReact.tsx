f0bbc5c9270d79054444f5fd3468ac98a4d7c73a
Emilio Navarro <sainfuw@outlook.com>
2024-06-30 20:28:07 -0400

    3 component added, Dialog, Select, AlertDialog

---
+++ src/pages/AlertDialog/_AlertDialogReact.tsx
@@ -0,0 +1,28 @@
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+
+export function AlertDialogReact() {
+  return (
+    <AlertDialog>
+      {/* Boton que hace el trigger */}
+      <AlertDialogTrigger asChild>
+        <Button variant="outline">Show Dialog</Button>
+      </AlertDialogTrigger>
+
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+
+        <AlertDialogFooter>
+          <AlertDialogCancel onClick={() => console.log("cancel")}>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={() => console.log("success")}>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  )
+}
